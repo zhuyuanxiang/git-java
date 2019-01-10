bf1955d294041a47b35f60d543e4d71c5a9da9cd
zYx.Tom <zhuyuanxiang@gmail.com>
2019-01-10 10:58:44 +0800

    c06.Replace Temp with Query is refactored

--- Refactoring-Fowler/src/c06/Price.java
+++ Refactoring-Fowler/src/c06/Price.java
@@ -5,14 +5,18 @@ public class Price {
   private int _itemPrice;
 
   double getPrice() {
-    int basePrice = _quantity * _itemPrice;
-    double discountFactor;
-    if (basePrice > 1000) {
-      discountFactor = 0.95;
-    } else {
-      discountFactor = 0.98;
-    }
-    return basePrice * discountFactor;
+    return basePrice() * discountFactor();
   }
 
+  private double discountFactor() {
+    if (basePrice() > 1000) {
+      return 0.95;
+    } else {
+      return 0.98;
+    }
+  }
+
+  private int basePrice() {
+    return _quantity * _itemPrice;
+  }
 }
