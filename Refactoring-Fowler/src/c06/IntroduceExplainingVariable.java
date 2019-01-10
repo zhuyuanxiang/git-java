f3f075824a8c9766352b300c3d17cb5dda575a2e
zYx.Tom <zhuyuanxiang@gmail.com>
2019-01-10 11:08:00 +0800

    c06.Introduce Explaining Variable is refacotred

--- Refactoring-Fowler/src/c06/IntroduceExplainingVariable.java
+++ Refactoring-Fowler/src/c06/IntroduceExplainingVariable.java
@@ -1,13 +1,14 @@
 package c06;
 
 public class IntroduceExplainingVariable {
-  private int _quantity;
-  private int _itemPrice;
+  private double _quantity;
+  private double _itemPrice;
 
   double price() {
     // price is base price - quantity discount + shipping
-    return ((_quantity * _itemPrice) -
-        (Math.max(0, _quantity - 500) * _itemPrice * 0.05)) +
-        Math.min(_quantity * _itemPrice * 0.1, 100.0);
+    final double basePrice = _quantity * _itemPrice;
+    final double quantityDiscount = Math.max(0, _quantity - 500) * _itemPrice * 0.05;
+    final double shipping = Math.min(basePrice * 0.1, 100.0);
+    return (basePrice - quantityDiscount) + shipping;
   }
 }
