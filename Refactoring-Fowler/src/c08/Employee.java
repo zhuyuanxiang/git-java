32754dd95a8d87544b8c916d55986be5e04d4f40
zYx.Tom <zhuyuanxiang@gmail.com>
2019-01-11 17:30:06 +0800

    c08.Replace Type Code with Subclasses is refactored

--- Refactoring-Fowler/src/c08/Employee.java
+++ Refactoring-Fowler/src/c08/Employee.java
@@ -1,12 +1,12 @@
 package c08;
 
-public class Employee {
+public abstract class Employee {
   private int _type;
   static final int ENGINEER = 0;
   static final int SALESMAN = 1;
   static final int MANAGER = 2;
 
-  Employee(int type) {
+  protected Employee(int type) {
     _type = type;
   }
 
@@ -14,7 +14,57 @@ public class Employee {
     _type = type;
   }
 
+  abstract int getType();
+
+  static Employee create(int type) {
+    switch (type) {
+      case ENGINEER:
+        return new Engineer();
+      case SALESMAN:
+        return new Salesman();
+      case MANAGER:
+        return new Manager();
+      default:
+        throw new IllegalArgumentException("Incorrect type code value");
+    }
+  }
+}
+
+
+class Engineer extends Employee {
+
+  @Override
   int getType() {
-    return _type;
+    return Employee.ENGINEER;
+  }
+
+  Engineer() {
+    super(ENGINEER);
+  }
+}
+
+
+class Salesman extends Employee {
+
+  @Override
+  int getType() {
+    return Employee.SALESMAN;
+  }
+
+  Salesman() {
+    super(SALESMAN);
+  }
+}
+
+
+class Manager extends Employee {
+
+  @Override
+  int getType() {
+    return Employee.MANAGER;
+  }
+
+  Manager() {
+    super(MANAGER);
   }
 }
