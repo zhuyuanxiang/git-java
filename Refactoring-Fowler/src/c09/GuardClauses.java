09333df78ff3aa63fb052b4b459b467ef25c24b4
zYx.Tom <zhuyuanxiang@gmail.com>
2019-01-12 18:38:44 +0800

    9.5 Replace Nested Conditional with Guard Clauses is refactored

--- Refactoring-Fowler/src/c09/GuardClauses.java
+++ Refactoring-Fowler/src/c09/GuardClauses.java
@@ -8,13 +8,13 @@ public class GuardClauses {
   private double _income;
 
   public double getAdjustedCapital() {
-    double result = 0.0;
-    if (_capital > 0.0) {
-      if ((_intRate > 0.0) && (_duration > 0.0)) {
-        result = (_income / _duration) * ADJ_FACTOR;
-      }
+    if (_capital < 0.0) {
+      return 0.0;
     }
-    return result;
+    if ((_intRate < 0.0) || (_duration < 0.0)) {
+      return 0.0;
+    }
+    return (_income / _duration) * ADJ_FACTOR;
   }
 
 }
