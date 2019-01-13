cfcc53bd2fb1484098f3fc2abeefe4c402e1824d
zYx.Tom <zhuyuanxiang@gmail.com>
2019-01-13 12:23:33 +0800

    10.4 Separate Query from Modifier refactored

--- Refactoring-Fowler/src/c10/seperateQueryFromModifier/People.java
+++ Refactoring-Fowler/src/c10/seperateQueryFromModifier/People.java
@@ -1,14 +1,18 @@
 package c10.seperateQueryFromModifier;
 
 public class People {
-  String foundMiscreant(String[] people) {
+  void foundMiscreant(String[] people) {
+    if (!foundPerson(people).equals("")) {
+      sendAlert();
+    }
+  }
+
+  String foundPerson(String[] people) {
     for (String element : people) {
       if (element.equals("Don")) {
-        sendAlert();
         return "Don";
       }
       if (element.equals("John")) {
-        sendAlert();
         return "John";
       }
     }
@@ -21,7 +25,9 @@ public class People {
   }
 
   void checkSEcurity(String people[]) {
-    String found = foundMiscreant(people);
+    // 编码中主要考虑的不是代码的效率，而是代码的易读性，效率可以在未来上线的时候再根据实际需要调整。
+    foundMiscreant(people);
+    String found = foundPerson(people);
     someLaterCode(found);
   }
 
